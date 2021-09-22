266293e3e0c26eee89311ad4f38a19431587b1df
losrobbos <losrobbos@web.de>
2021-09-22 10:14:39 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+import dotenv from 'dotenv'
+
+dotenv.config() // load .env contents into process.env
---
+++ db-connect.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const MONGO_URI = process.env.MONGO_URI
+
+console.log("DB URI:", MONGO_URI)
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(() => console.log("Connection to database established!"))
+.catch((err) => console.log("[ERROR] Connection failed!"))
